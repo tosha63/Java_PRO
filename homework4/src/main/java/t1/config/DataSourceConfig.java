ba684e643b0e84dc68c7c87d0008026f520c68bb
ASHtanko <ASHtanko@inno.tech>
2025-04-21 12:11:13 +0300

    Fix

--- homework4/src/main/java/t1/config/DataSourceConfig.java
+++ homework4/src/main/java/t1/config/DataSourceConfig.java
@@ -2,20 +2,32 @@ package t1.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource(value = "classpath:application.properties")
 public class DataSourceConfig {
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+
     @Bean
     public DataSource dataSource() {
         var hikariConfig = new HikariConfig();
-        hikariConfig.setDriverClassName("org.postgresql.Driver");
-        hikariConfig.setJdbcUrl("jdbc:postgresql://localhost:25432/postgres");
-        hikariConfig.setUsername("test");
-        hikariConfig.setPassword("test");
+        hikariConfig.setDriverClassName(driverClassName);
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
         return new HikariDataSource(hikariConfig);
     }
 }
