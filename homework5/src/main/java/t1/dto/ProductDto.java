f80cbfbd523608034f8b9c9162337570489d7539
ASHtanko <ASHtanko@inno.tech>
2025-05-02 18:57:15 +0300

    Spring Web MVC

---
+++ homework5/src/main/java/t1/dto/ProductDto.java
@@ -0,0 +1,20 @@
+package t1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import t1.entity.ProductType;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class ProductDto {
+    private String accountNumber;
+    private BigDecimal balance;
+    private ProductType productType;
+    private Long userId;
+}
---
+++ homework5/src/main/java/t1/dto/UserDto.java
@@ -0,0 +1,15 @@
+package t1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class UserDto {
+    private String username;
+    private Integer age;
+}
